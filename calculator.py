1dfbcc12e314b00b60c421a104d2a3c3f6e9f492
Clément Savalle <savalle.clement@gmail.com>
2020-05-04 18:24:44 +1200

    use of Glade for Gtk app

--- calculator.py
+++ calculator.py
@@ -1,62 +1,21 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 
-from dataclasses import dataclass, field
-import data
+class Handler:
+    def onDestroy(self, *args):
+        Gtk.main_quit()
 
+    def onButtonPressed(self, button):
+        print("Hello World!")
 
-@dataclass
-class Rider():
-    """Represent a rider.
+builder = Gtk.Builder()
+builder.add_from_file("din_calc.glade")
+builder.connect_signals(Handler())
 
-    keyword arguments are obvious.
-    """
-    name: str = "Rider"
-    age: int = 30
-    height: int = 175
-    weight: int = 75
-    level: int = 2
-    sole_length: int = 300
-    din_set: float = field(init=False)
+window = builder.get_object("window1")
+window.show_all()
 
-    def __post_init__(self):
-        self.din_set = self.get_din_set()
-
-    def increment_din_code(self, code, increment):
-        """Increment a code in the DIN codes list."""
-        codes_list = ''.join(data.DIN_TAB.keys())
-        return codes_list[codes_list.index(code) + increment]
-
-    def get_din_set(self):
-        """Return the DIN setting of the rider."""
-
-        code_height = 'A'
-        code_weight = 'A'
-        for code, sizes in data.WEIGHT_DICT.items():
-            if sizes[0] < self.weight < sizes[1]:
-                code_weight = code
-                break
-
-        for code, sizes in data.HEIGHT_DICT.items():
-            if sizes[0] < self.height < sizes[1]:
-                code_height = code
-                break
-
-        code_din = max(code_height, code_weight)
-
-        if self.age < 10 or self.age > 49.1:
-            code_din = self.increment_din_code(code_din, -1)
-
-        code_din = self.increment_din_code(code_din, self.level-2)
-
-        for sole_lenght_sup in data.SOLE_LENGHT_TAB:
-            if self.sole_length < sole_lenght_sup:
-                sole_lenght_index = data.SOLE_LENGHT_TAB.index(sole_lenght_sup)
-                break
-
-        return data.DIN_TAB.get(code_din)[sole_lenght_index]
-
-
-if __name__ == '__main__':
-    rider = Rider()
-    rider.din_set = rider.get_din_set()
-    print(rider.din_set)
+Gtk.main()
--- data.py
+++ data.py
@@ -2,6 +2,8 @@
 
 """
 
+from dataclasses import dataclass, field
+
 DIN_TAB = {
     'A': [0.75, 0.75, 0.00, 0.00, 0.00, 0.00],
     'B': [1.00, 1.00, 0.75, 0.00, 0.00, 0.00],
@@ -56,3 +58,62 @@ LEVELS = (
     (3, 'Advanced'),
     (4, 'Expert'),
 )
+
+
+def increment_din_code(self, code, increment):
+    """Increment a code in the DIN codes list."""
+    codes_list = ''.join(DIN_TAB.keys())
+    return codes_list[codes_list.index(code) + increment]
+
+
+@dataclass
+class Rider():
+    """Represent a rider.
+
+    keyword arguments are obvious.
+    """
+    name: str = "Rider"
+    age: int = 30
+    height: int = 175
+    weight: int = 75
+    level: int = 2
+    sole_length: int = 300
+    din_set: float = field(init=False)
+
+    def __post_init__(self):
+        self.din_set = self.get_din_set()
+
+    def get_din_set(self):
+        """Return the DIN setting of the rider."""
+
+        code_height = 'A'
+        code_weight = 'A'
+        for code, sizes in WEIGHT_DICT.items():
+            if sizes[0] < self.weight < sizes[1]:
+                code_weight = code
+                break
+
+        for code, sizes in HEIGHT_DICT.items():
+            if sizes[0] < self.height < sizes[1]:
+                code_height = code
+                break
+
+        code_din = max(code_height, code_weight)
+
+        if self.age < 10 or self.age > 49.1:
+            code_din = increment_din_code(code_din, -1)
+
+        code_din = ncrement_din_code(code_din, self.level-2)
+
+        for sole_lenght_sup in SOLE_LENGHT_TAB:
+            if self.sole_length < sole_lenght_sup:
+                sole_lenght_index = SOLE_LENGHT_TAB.index(sole_lenght_sup)
+                break
+
+        return DIN_TAB.get(code_din)[sole_lenght_index]
+
+
+if __name__ == '__main__':
+    rider = Rider()
+    rider.din_set = rider.get_din_set()
+    print(rider.din_set)
