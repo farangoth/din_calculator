7739e154e34b7cf9a2a0078b6f4f19d5657b4b98
Clément Savalle <savalle.clement@gmail.com>
2020-05-05 15:38:13 +1200

    add an About Dialog

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import gi
 
 from data import Rider
@@ -8,14 +10,18 @@ from gi.repository import Gtk  # noqa: E402
 
 class Din_window(Gtk.Window):
 
-    def on_destroy(self, *args):
+    def on_main_window_destroy(self, *args):
         """Link the close button and the end of the Gtk process."""
-        print("quitting")
+        print("quit from button")
         Gtk.main_quit()
 
     def on_gtk_quit_activate(self, menuitem):
         print("Quit from the menu")
         Gtk.main_quit()
+
+    def on_gtk_about_activate(self, menuitem):
+        self.response = self.aboutdialog.run()
+        self.aboutdialog.hide()
 
     def on_button_yes_clicked(self, button):
         self.rider = Rider()
@@ -43,7 +49,9 @@ class Din_window(Gtk.Window):
         self.builder.connect_signals(self)
 
         self.window = self.builder.get_object("main_window")
-        self.window.show_all()
+        self.aboutdialog = self.builder.get_object("aboutdialog")
+
+        self.window.show()
 
 
 if __name__ == "__main__":
