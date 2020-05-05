ad7667edc4a231474399e822ffd6450e8695b0ee
Clément Savalle <savalle.clement@gmail.com>
2020-05-05 14:57:38 +1200

    get the calc actually doing something

--- calculator.py
+++ calculator.py
@@ -1,21 +1,51 @@
 import gi
 
+from data import Rider
+
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk  # noqa: E402
 
 
-class Handler:
-    def onDestroy(self, *args):
+class Din_window(Gtk.Window):
+
+    def on_destroy(self, *args):
+        """Link the close button and the end of the Gtk process."""
+        print("quitting")
         Gtk.main_quit()
 
-    def onButtonPressed(self, button):
-        print("Hello World!")
+    def on_gtk_quit_activate(self, menuitem):
+        print("Quit from the menu")
+        Gtk.main_quit()
 
-builder = Gtk.Builder()
-builder.add_from_file("din_calc.glade")
-builder.connect_signals(Handler())
+    def on_button_yes_clicked(self, button):
+        self.rider = Rider()
+        self.din_set_label = self.builder.get_object("din_result")
 
-window = builder.get_object("window1")
-window.show_all()
+        self.rider.weight = int(self.builder.get_object("weight").get_text())
+        self.rider.height = int(self.builder.get_object("height").get_text())
+        self.rider.level = int(self.builder.get_object("level").get_text())
+        self.rider.age = int(self.builder.get_object("age").get_text())
+        self.rider.sole_length = int(self.builder.get_object(
+                                     "sole_length").get_text())
 
-Gtk.main()
+        self.rider.din_set = self.rider.get_din_set()
+
+        print(self.rider)
+
+        self.din_set_label.set_text(str(self.rider.din_set))
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="DIN Calc")
+        self.gladefile = "din_calc.glade"
+        self.builder = Gtk.Builder()
+
+        self.builder.add_from_file(self.gladefile)
+        self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("main_window")
+        self.window.show_all()
+
+
+if __name__ == "__main__":
+    main = Din_window()
+    Gtk.main()
